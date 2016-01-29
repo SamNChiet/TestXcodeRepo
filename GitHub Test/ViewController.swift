6189d9d0c04da117dd66ee866dfcea2d00240200
Sam Chiet <samnchiet@gmail.com>
2016-01-28 20:01:35 -0800

    Initial Commit

---
+++ GitHub Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Test
+//
+//  Created by Sam Chiet on 1/28/16.
+//  Copyright © 2016 Sam Chiet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
